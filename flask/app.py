7edfeb5cefa1635d01ad00a3c960666c4143c67f
B.Lakshmi Hari Prasanna <50226095+blhprasanna99@users.noreply.github.com>
2021-05-11 12:31:33 +0530

    Add files via upload

---
+++ flask/app.py
@@ -0,0 +1,132 @@
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+import cv2
+
+# Keras
+from keras.models import Sequential 
+from keras.layers import Conv2D, MaxPooling2D 
+from keras.layers import Activation, Dropout, Flatten, Dense 
+from keras import backend as K 
+  
+img_width, img_height = 224, 224
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+# Model saved with Keras model.save()
+MODEL_PATH = 'models/plant_seedling_saved.h5'
+
+
+if K.image_data_format() == 'channels_first': 
+    input_shape = (3, img_width, img_height) 
+else: 
+    input_shape = (img_width, img_height, 3) 
+
+
+model = Sequential() 
+model.add(Conv2D(32, (2, 2), input_shape = input_shape)) 
+model.add(Activation('relu')) 
+model.add(MaxPooling2D(pool_size =(2, 2))) 
+  
+model.add(Conv2D(32, (2, 2))) 
+model.add(Activation('relu')) 
+model.add(MaxPooling2D(pool_size =(2, 2))) 
+  
+model.add(Conv2D(64, (2, 2))) 
+model.add(Activation('relu')) 
+model.add(MaxPooling2D(pool_size =(2, 2))) 
+  
+model.add(Flatten()) 
+model.add(Dense(64)) 
+model.add(Activation('relu')) 
+model.add(Dropout(0.5)) 
+model.add(Dense(1)) 
+model.add(Activation('sigmoid')) 
+
+  
+
+# Load your trained model
+model.load_weights(MODEL_PATH)
+
+#model._make_predict_function()          # Necessary
+# print('Model loaded. Start serving...')
+
+# You can also use pretrained model from Keras
+# Check https://keras.io/applications/
+#from keras.applications.resnet50 import ResNet50
+#model = ResNet50(weights='imagenet')
+#model.save('')
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+"""
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(224, 224))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    # x = np.true_divide(x, 255)
+    x = np.expand_dims(x, axis=0)
+
+    # Be careful how your trained model deals with the input
+    # otherwise, it won't make correct prediction!
+    x = preprocess_input(x, mode='caffe')
+
+    preds = model.predict(x)
+    return preds
+"""
+
+#import matplotlib.pyplot as plt 
+from tensorflow.keras.preprocessing import image
+
+def predict(test_path,model):
+    test_image=image.load_img(test_path,target_size=(img_width,img_height))
+    test_image=image.img_to_array(test_image)
+    test_image=np.expand_dims(test_image,axis=0)
+    result=model.predict(test_image)
+    result=int(result)
+    k=['plant','weed']
+    return k[result]
+        
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = predict(file_path, model)
+
+        # Process your result for human
+        # pred_class = preds.argmax(axis=-1)            # Simple argmax
+        """"pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+        result = str(pred_class[0][0][1])             # Convert to string"""
+        return preds
+    return None
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
